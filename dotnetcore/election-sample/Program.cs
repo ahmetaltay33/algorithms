ec02db17e9a0e54ac192b3012fd641d0fd35841b
Ahmet Altay <ahmetaltay33@users.noreply.github.com>
2020-04-11 19:00:48 +0300

    initial commit

---
+++ dotnetcore/election-sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace election_sample
+{
+    class Oy
+    {
+        public int Id { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string Mahalle { get; set; }
+        public string Okul { get; set; }
+        public int SandikNo { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İşlemler başlıyor...");
+
+
+
+            Console.WriteLine("İşlemler tamamlandı. Çıkmak için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
